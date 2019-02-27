10486179c20a7d9212c4386ad5da040eb55c5691
sl33pw4lking <48050772+sl33pw4lking@users.noreply.github.com>
2019-02-27 10:34:07 +0100

    Add files via upload
    array arithmetic in for loop

---
+++ calc.java
@@ -0,0 +1,18 @@
+public class calc{
+    public static void main(String[] args) {
+        int [][] example = {{2,8},{7,8}};
+        int x = example[0][0];
+
+
+
+        for (int i = 1; i<=x;)
+
+
+        System.out.println(example.length);
+        System.out.println("blubb" + x + "breaks the system");
+
+
+
+
+    }
+}
